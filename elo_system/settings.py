f801bc37ef1370473958423d18fe3901f6a72b19
Haukur Stefansson <haukur@temposoftware.com>
2016-05-23 13:08:24 +0000

    Uncomment database settings

--- elo_system/settings.py
+++ elo_system/settings.py
@@ -83,12 +83,12 @@ STATICFILES_DIRS = [
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#     }
-# }
-# DATABASES['default'].update(db_from_env)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    }
+}
+DATABASES['default'].update(db_from_env)
 
 
 # Internationalization
