537e66fae8c291afc1052ea4f961f78dcf1819b8
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2016-05-23 16:03:10 +0000

    Fix startup on clean slate

--- elo_system/settings.py
+++ elo_system/settings.py
@@ -12,11 +12,9 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+DEBUG = False
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
